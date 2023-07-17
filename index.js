fb0c8328b08849b005d48734079780e81ed8220c
chrislange89 <chris_lange_89@hotmail.com>
2023-07-18 07:13:20 +1000

    add home page

--- index.js
+++ index.js
@@ -5,27 +5,32 @@ const morgan = require('morgan');
 
 const app = express();
 
+// routers
 const personRouter = require('./routes/persons.route');
 const infoRouter = require('./routes/info.route');
 
+// config
 dotenv.config();
 morgan.token('body', (req) => JSON.stringify(req.body));
 const morganFormat = ':method :url :status :res[content-length] - :response-time ms :body';
 
+// middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan(morganFormat));
 
+// routes
 app.use('/api/persons', personRouter);
 app.use('/info', infoRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(`${__dirname}/html/links.html`);
 });
 
 const PORT = process.env.PORT || 3000;
 
+// start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
