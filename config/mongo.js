bc403e06714c838dd6856978ae5ea5666a5e950f
chrislange89 <chris_lange_89@hotmail.com>
2023-07-19 09:12:38 +1000

    create mongo connection

--- config/mongo.js
+++ config/mongo.js
@@ -1,2 +1,10 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const mongoConnectionString = process.env.MONGO_URI;
+
+mongoose.connect(mongoConnectionString)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('Could not connect to MongoDB', err));
