58e832c26233154c20d47d9a11a05c24c8e5fa68
chrislange89 <chris_lange_89@hotmail.com>
2023-07-19 11:03:13 +1000

    create test mongoose file

--- config/mongo.js
+++ config/mongo.js
@@ -3,8 +3,40 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const mongoConnectionString = process.env.MONGO_URI;
+const passwordToUse = (process.argv.length > 2) ? process.argv[2] : process.env.MONGO_PASSWORD;
+const mongoConnectionString = process.env.MONGO_URI.replace('<password>', passwordToUse);
 
 mongoose.connect(mongoConnectionString)
-  .then(() => console.log('Connected to MongoDB'))
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
   .catch((err) => console.log('Could not connect to MongoDB', err));
+
+const db = mongoose.connection;
+
+const phonebookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Phonebook = mongoose.model('Phonebook', phonebookSchema);
+
+if (process.argv[3] === undefined || process.argv[4] === undefined) {
+  Phonebook.find({}).then((result) => {
+    console.log('Phonebook:');
+    result.forEach((personFound) => {
+      console.log(`${personFound.name} ${personFound.number}`);
+    });
+    db.close();
+    process.exit(1);
+  });
+} else {
+  const person = new Phonebook({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+  person.save().then((result) => {
+    console.log(`Added name: '${result.name}' with number: '${result.number}' to phonebook`);
+    db.close();
+  });
+}
